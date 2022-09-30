facd1fed8e7dbbf93d3f4f171f3b223907d251fa
TimmyDenon <popomei9999@gmail.com>
2022-09-30 14:46:38 +0800

    2.21

---
+++ HM1 2.21/source/Main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// past 9*9 rectangle
+int main(void)
+{
+	printf("\t*********\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*********\n\n");
+	printf("/////////////////////////\n\n");
+// past oval
+	printf("\t   ***   \n");
+	printf("\t *     * \n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t*       *\n");
+	printf("\t *     * \n");
+	printf("\t   ***   \n\n");
+// past arrow
+	printf("/////////////////////////\n\n");
+	printf("\t    *    \n");
+	printf("\t   ***   \n");
+	printf("\t  *****  \n");
+	printf("\t    *    \n");
+	printf("\t    *    \n");
+	printf("\t    *    \n");
+	printf("\t    *    \n");
+	printf("\t    *    \n");
+	printf("\t    *    \n\n");
+	printf("/////////////////////////\n\n");
+//past diamond
+	printf("\t    *    \n");
+	printf("\t   * *   \n");
+	printf("\t  *   *  \n");
+	printf("\t *     * \n");
+	printf("\t*       *\n");
+	printf("\t *     * \n");
+	printf("\t  *   *  \n");
+	printf("\t   * *  \n");
+	printf("\t    *     \n\n");
+}
