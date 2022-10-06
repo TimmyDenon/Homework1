d8377e1adb432a30efc095607690fbf8865d9443
TimmyDenon <popomei9999@gmail.com>
2022-10-06 20:55:33 +0800

    debug

--- HM1 2.23/source/Main.c
+++ HM1 2.23/source/Main.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int x, y, z;
+int x, y, z,t;
 
 int main()
 {
-	int integer[] = { x,y,z };
 	scanf_s("%d %d %d", &x, &y, &z);
-	for(int i=0 ;i< 2 ;i++)
-	{
-		if (integer[i] > integer[i + 1])
-			integer[i + 1] = integer[i];
-		for (int j = 0; j < 1; j++)
-		{
-			if (integer[i] > integer[i + 1])
-				integer[i + 1] = integer[i];
-		}
-	}
-	printf("%d %d", x, z);
+	if (x > y && x > z)
+		printf("largest is %d\n\n", x);
+	if (y > x && y > z)
+		printf("largest is %d\n\n", y);
+	if (z > y && z > x)
+		printf("largest is %d\n\n", z);
+
+	if (x < y && x < z)
+		printf("smallest is %d\n", x);
+	if (y < x && y < z)
+		printf("smallest is %d\n", y);
+	if (z < y && z < x)
+		printf("smallest is %d\n", z);
 }
